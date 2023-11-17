81e74bb00ee01be95f304f49bc344830946098f3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-11-17 23:30:27 +0000

    X6739: Reboot to recovery if init fails
    Change-Id: I1d8f38620caa7b003911ff30f69ed7973c100e6d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 
+BOARD_BOOTCONFIG := androidboot.init_fatal_reboot_target=recovery
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := x6739u_h931
 TARGET_NO_BOOTLOADER := true

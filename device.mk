492f9cfb76728d03151f6f386845f64074c689c8
bengris32 <bengris32@protonmail.ch>
2023-11-18 21:05:23 +0000

    X6739: Configure Bluetooth stack
    Change-Id: Ifc9671bebbf4456003e3e211822289885df89045
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -61,6 +61,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio-impl
+
 # Boot Control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl \

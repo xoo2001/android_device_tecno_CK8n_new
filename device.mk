e482faa6a9a36a7f6cc0a167a28e385e369c9253
bengris32 <bengris32@protonmail.ch>
2023-11-25 21:52:05 +0000

    X6739: Add dynamic sensor HAL support
    Change-Id: Ica3683c14dff3b603f369ebef65a276d4cdcb068
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -204,6 +204,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
+PRODUCT_PACKAGES += \
+    sensors.dynamic_sensor_hal
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 

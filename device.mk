a32faab7ad6d2caebeae6caef4d160007efdd289
bengris32 <bengris32@protonmail.ch>
2024-02-14 13:02:00 +0000

    X6739: Build OSS MediaTek thermal service
    Change-Id: I6856b74a187ae735f7b2d8c54c43c0e54d31b4ef
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Co-Authored-By: Woomymy <woomy@woomy.be>
    Signed-off-by: AndroidHQ254 <d.murimi14@gmail.com>

--- device.mk
+++ device.mk
@@ -350,6 +350,13 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/mediatek
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \

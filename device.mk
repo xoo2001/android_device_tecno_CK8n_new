7aa933cf1d5849e5a858f177a0b00b64e504348e
bengris32 <bengris32@protonmail.ch>
2024-03-23 22:01:57 +0000

    X6739: Build `PowerOffAlarm`
    Change-Id: I69537d39e3213822070ff80056285882a1de11c5
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -293,6 +293,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Power-off Alarm
+PRODUCT_PACKAGES += \
+    PowerOffAlarm
+
 # Properties
 include $(LOCAL_PATH)/vendor_logtag.mk
 

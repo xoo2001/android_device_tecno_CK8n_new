9851eb0005c9397a85765fee1ecd7a868352ae61
Aaron Kling <webgeek1234@gmail.com>
2024-09-12 14:09:15 +0530

    X6739: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,6 +25,14 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+	cat <<EOF >>"$1"
+		"device/infinix/X6739",
+		"hardware/mediatek",
+		"hardware/mediatek/libmtkperf_client"
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 

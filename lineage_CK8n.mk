e28759b63f78bed9d42b6fd843190216bb5b9fa7
xoo2001 <ekora2001@gmail.com>
2025-09-11 12:35:09 +0700

    CK8n: Rebrand dt from X6739
    Signed-off-by: xoo2001 <ekora2001@gmail.com>

---
+++ lineage_CK8n.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from CK8n device
+$(call inherit-product, device/tecno/CK8n/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_CK8n
+PRODUCT_DEVICE := CK8n
+PRODUCT_MANUFACTURER := TECNO
+PRODUCT_BRAND := Tecno
+PRODUCT_MODEL := Tecno CK8n
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="sys_tssi_64_armv82_tecno-user 14 UP1A.231005.007 732551 release-keys" \
+    BuildFingerprint=TECNO/CK8n-GL/TECNO-CK8n:14/UP1A.231005.007/241029V472:user/release-keys \
+    SystemDevice=Tecno-CK8n \
+    SystemName=CK8n-GL

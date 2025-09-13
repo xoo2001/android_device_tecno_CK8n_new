7a4c066dd9e879227a283a528d0d51106e1c9a97
xoo2001 <ekora2001@gmail.com>
2025-09-13 16:17:02 +0700

    CK8n: Migrate to py extract utils
    Signed-off-by: xoo2001 <ekora2001@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/tecno/CK8n',
+    'hardware/mediatek',
+    'hardware/mediatek/libmtkperf_client',
+    'hardware/transsion',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/lib64/libsink.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so'),
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+    ('system_ext/etc/init/init.vtservice.rc', 'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc'): blob_fixup()
+        .regex_replace('start', 'enable'),
+    'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()
+        .add_line_if_missing('    interface android.hardware.media.c2@1.0::IComponentStore default')
+        .add_line_if_missing('    interface android.hardware.media.c2@1.1::IComponentStore default')
+        .add_line_if_missing('    interface android.hardware.media.c2@1.2::IComponentStore default'),
+    ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+    ('vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron', 'vendor/bin/nfcstackp-vendor', 'vendor/lib64/libsysenv.so', 'vendor/lib64/libtflite_mtk.so', 'vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/bin/hw/vendor.mediatek.hardware.tranHwInfo@1.0-service', 'vendor/bin/meta_tst'): blob_fixup()
+        .add_needed('libbase_shim.so'),
+    'vendor/bin/hw/camerahalserver': blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v31.so')
+        .replace_needed('libbinder.so', 'libbinder-v31.so')
+        .replace_needed('libutils.so', 'libutils-v31.so')
+        .add_needed('libhidlbase_shim.so')
+        .add_needed('libprocessgroup_shim.so'),
+    ('vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service', 'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v31.so'),
+    ('vendor/bin/mnld', 'vendor/lib/libaalservice.so', 'vendor/lib64/libaalservice.so', 'vendor/lib/librgbwlightsensor.so', 'vendor/lib64/librgbwlightsensor.so', 'vendor/lib64/libcam.utils.sensorprovider.so', 'vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so'): blob_fixup()
+        .add_needed('android.hardware.sensors@1.0-convert-shared.so'),
+    'vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc': blob_fixup()
+        .regex_replace('@1.2-mediatek', '@1.2-mediatek-64b'),
+    'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
+        .regex_replace('1.1', '1.2'),
+    'vendor/lib64/hw/audio.primary.mt6893.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v31.so')
+        .replace_needed('libalsautils.so', 'libalsautils-v31.so')
+        .add_needed('libstagefright_foundation-v33.so'),
+    'vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v31.so')
+        .replace_needed('libutils.so', 'libutils-v31.so')
+        .add_needed('libcamera_metadata_shim.so'),
+    'vendor/lib64/hw/hwcomposer.mt6893.so': blob_fixup()
+        .patchelf_version('0_17_2')
+        .add_needed('libprocessgroup_shim.so'),
+    ('vendor/lib64/lib3a.ae.stat.so', 'vendor/lib64/lib3a.flash.so', 'vendor/lib64/lib3a.sensors.color.so', 'vendor/lib64/lib3a.sensors.flicker.so', 'vendor/lib64/libaaa_ltm.so', 'vendor/lib64/libSQLiteModule_VER_ALL.so'): blob_fixup()
+        .add_needed('liblog.so'),
+    'vendor/lib64/mt6893/libmnl.so': blob_fixup()
+        .add_needed('libcutils.so'),
+    ('vendor/lib64/libsegmentionPre.so', 'vendor/lib64/libMegviiHum.so', 'vendor/lib64/libtranssion_bodybeauty.so'): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'CK8n',
+    'tecno',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()

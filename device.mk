6bae32e487b67f15c8a4f8f10d2110bb734c98e6
xoo2001 <ekora2001@gmail.com>
2025-10-15 17:07:10 +0700

    CK8n: drop FM Radio

--- device.mk
+++ device.mk
@@ -131,10 +131,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
-# FM Radio
-PRODUCT_PACKAGES += \
-    FMRadio
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
